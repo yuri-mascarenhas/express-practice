22041f3c65d9bdccbf16e6305a9e4ab86fc2e778
yurism <mascarenhasyuri@hotmail.com>
2024-03-16 22:47:58 -0300

    [Add] get different routing

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import express from "express";
 
 const app = express()
 
-app.use(() => {
-    console.log("Use request")
+app.get("/", (req, res) => {
+    res.send("Welcome to the homepage")
+})
+app.get("/collaborators", (req, res) => {
+    res.send([{name: "John Doe", company_id: 233, position: "Analyst"}, {name: "Gene Doe", company_id: 18, position: "Director Assistent"}])
+})
+app.get("/supply", (req, res) => {
+    res.send([{type: "Notebook", collaborator: 18}])
 })
 
 app.listen(3000, () => {
